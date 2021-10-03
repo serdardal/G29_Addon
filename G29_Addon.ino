36e41705136861519975510b8da46253c0b36043
Serdar Dal <serdardal@users.noreply.github.com>
2021-10-03 13:09:29 +0300

    joystick library and some cleanup

--- G29_Addon.ino
+++ G29_Addon.ino
@@ -1,88 +1,56 @@
-int sensorValue;
-int minValue = 150;
-int maxValue = 500;
+#include <Joystick.h>
 
-int pos0Value;
-int pos1Value;
-int pos2Value;
-int pos3Value;
+int retarderSensorValue;
+int retarderPositionValues[] = {584, 542, 499, 464};
 
-int currentPos = 0;
+int retarderCurrentPos = 0;
+int retarderPotValueMargin = 7;
 
-int tolerance = 10;
-
-// the setup routine runs once when you press reset:
 void setup() {
-	// initialize serial communication at 9600 bits per second:
-	Serial.begin(9600);
-	setPosValues();
-  Serial.print("Current pos: ");
-  Serial.println(currentPos);
+  Joystick.begin();
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-	// read the input on analog pin 0:
-	sensorValue = analogRead(A0);
-	// print out the value you read:
-	//Serial.println(sensorValue);
-  int newPos = getNewPos(sensorValue);
-  if(newPos >= 0 && newPos != currentPos){
-    if(newPos - currentPos > 0){
-      increasePos(newPos);
-    }else{
-      decreasePos(newPos);
+  retarderSensorValue = analogRead(A0);
+
+  int newRetarderPos = getNewRetarderPos(retarderSensorValue);
+
+  if (newRetarderPos >= 0 && newRetarderPos != retarderCurrentPos) {
+    adjustRetarderPosition(newRetarderPos);
+  }
+}
+
+int getNewRetarderPos(int value) {
+  for (int i = 0; i < 4 ; i++) {
+    if (abs(value - retarderPositionValues[i]) < retarderPotValueMargin) {
+      return i;
     }
   }
-  
-	delay(10);        // delay in between reads for stability
+  return -1;
 }
 
-void setPosValues() {
-	pos0Value = minValue;
-	pos3Value = maxValue;
-	
-	int difference = maxValue - minValue;
-	int range = difference / 3 ;
+void adjustRetarderPosition(int newPos) {
+  bool turnDirection = newPos > retarderCurrentPos;
+  int diff = abs(newPos - retarderCurrentPos);
+  for (int i = 0; i < diff; i++) {
 
-  pos1Value = minValue + range;
-  pos2Value = maxValue - range;
-}
+    if (turnDirection) {
+      ++retarderCurrentPos;
+      pushToButton(0);
+    } else {
+      --retarderCurrentPos;
+      pushToButton(1);
+    }
 
-int getNewPos(int value) {
-  if(abs(value - pos0Value) < tolerance){
-    return 0;
-  }else if(abs(value - pos1Value) < tolerance){
-    return 1;  
-  }else if(abs(value - pos2Value) < tolerance){
-    return 2;  
-  }else if(abs(value - pos3Value) < tolerance){
-    return 3;
-  }else return -1;
-}
-
-void increasePos(int newPos) {
-  int diff = newPos - currentPos;
-  for(int i = 0; i < diff; i++){
-      Serial.print("Position increased. Current pos: ");
-      Serial.println(++currentPos);
-
-      if(i >= 0 && i < (diff - 1)){
-        Serial.println("waiting...");
-        delay(1000);
-      }
+    if (i >= 0 && i < (diff - 1)) {
+      // delay between push button if diff > 1
+      delay(50);
+    }
   }
 }
 
-void decreasePos(int newPos) {
-  int diff = currentPos - newPos;
-  for(int i = 0; i < diff; i++){
-      Serial.print("Position decreased. Current pos: ");
-      Serial.println(--currentPos);
-
-      if(i >= 0 && i < (diff - 1)){
-        Serial.println("waiting...");
-        delay(1000);
-      }
-  }  
+void pushToButton(int buttonNum) {
+  Joystick.pressButton(buttonNum);
+  delay(10);
+  Joystick.releaseButton(buttonNum);
 }
